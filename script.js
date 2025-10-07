127211bdf115fccf57d114786de332a2f6c64fb4
kalai02484 <kalai02484@gmail.com>
2025-10-07 21:52:47 +0530

    completed till subscribe form

--- script.js
+++ script.js
@@ -1,37 +1,81 @@
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+var swiper = new Swiper(".deal-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 40,
+  loop: true,
+  //   autoplay: {
+  //    delay: 5000,
+  //  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
     },
-    breakpoints: {
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-        },
-      },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1280: {
+      slidesPerView: 4,
+    },
+  },
 });
 
+var swiper = new Swiper(".vacation-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 40,
+  loop: true,
+  //   autoplay: {
+  //    delay: 5000,
+  //  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
 
-
-// var swiper = new Swiper(".mySwiper", {
-//       slidesPerView: 3,
-//       spaceBetween: 30,
-//       pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//       },
-//     });
+var swiper = new Swiper(".blog-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 40,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1280: {
+      slidesPerView: 4,
+    },
+  },
+});
